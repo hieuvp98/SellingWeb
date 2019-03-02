ecdb46d0a34e9dad01eba7d6c018fc19cc2e7fc4
hieuvp98 <38047984+hieuvp98@users.noreply.github.com>
2019-03-02 10:50:32 +0700

    fix filter chain again

--- src/main/java/com/bksoftware/sellingweb/controller/AdminController.java
+++ src/main/java/com/bksoftware/sellingweb/controller/AdminController.java
@@ -2,20 +2,24 @@ package com.bksoftware.sellingweb.controller;
 
 import com.bksoftware.sellingweb.entities.AppAdmin;
 import com.bksoftware.sellingweb.repository.AppAdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping(value = "/api/admin")
 public class AdminController {
     private final AppAdminRepository appAdminRepository;
-
-    public AdminController(AppAdminRepository appAdminRepository) {
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    public AdminController(AppAdminRepository appAdminRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.appAdminRepository = appAdminRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
+
     @GetMapping(value = "/info")
     public ResponseEntity<AppAdmin> getInfo(){
         AppAdmin appAdmin = appAdminRepository.findAll().get(0);
@@ -23,5 +27,18 @@ public class AdminController {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
         else return new ResponseEntity<>(appAdmin,HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/change-password",params = {"old","new"})
+    public ResponseEntity<String> changePassword(@RequestParam(value = "old") String oldPassword,
+                                                 @RequestParam(value = "new") String newPassword, HttpServletRequest request){
+        String username = request.getUserPrincipal().getName();
+        AppAdmin appAdmin = appAdminRepository.findByUsername(username);
+        if (bCryptPasswordEncoder.matches(oldPassword,appAdmin.getPassword())){
+            appAdmin.setPassword(bCryptPasswordEncoder.encode(newPassword));
+            appAdminRepository.save(appAdmin);
+            return new ResponseEntity<>("change success",HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<>("password is not correct",HttpStatus.BAD_REQUEST);
     }
 }
