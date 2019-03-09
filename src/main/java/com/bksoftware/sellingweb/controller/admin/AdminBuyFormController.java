15675750eaff3a36f30161b5c9b6cc2d9af22ca1
hieuvp98 <38047984+hieuvp98@users.noreply.github.com>
2019-03-09 16:50:37 +0700

    fix something

--- src/main/java/com/bksoftware/sellingweb/controller/admin/AdminBuyFormController.java
+++ src/main/java/com/bksoftware/sellingweb/controller/admin/AdminBuyFormController.java
@@ -1,8 +1,10 @@
 package com.bksoftware.sellingweb.controller.admin;
 
+import com.bksoftware.sellingweb.entities.UserMail;
 import com.bksoftware.sellingweb.entities.product.BuyForm;
 import com.bksoftware.sellingweb.entities.product.Product;
 import com.bksoftware.sellingweb.entities.product.ProductDetails;
+import com.bksoftware.sellingweb.service_impl.SendMailService_Impl;
 import com.bksoftware.sellingweb.service_impl.product.BuyFormService_Impl;
 import com.bksoftware.sellingweb.service_impl.product.ProductDetailsService_Impl;
 import com.bksoftware.sellingweb.service_impl.product.ProductService_Impl;
@@ -21,10 +23,14 @@ import java.util.stream.Collectors;
 public class AdminBuyFormController {
     private final BuyFormService_Impl buyFormService;
     private final ProductDetailsService_Impl productDetailsService;
+    private final UserMail userMail;
+    private final SendMailService_Impl sendMailService;
 
-    public AdminBuyFormController(BuyFormService_Impl buyFormService, ProductDetailsService_Impl productDetailsService, ProductService_Impl productService) {
+    public AdminBuyFormController(BuyFormService_Impl buyFormService, ProductDetailsService_Impl productDetailsService, ProductService_Impl productService, UserMail userMail, SendMailService_Impl sendMailService) {
         this.buyFormService = buyFormService;
         this.productDetailsService = productDetailsService;
+        this.userMail = userMail;
+        this.sendMailService = sendMailService;
     }
 
     @GetMapping("/uncheck-buy-form")
@@ -47,6 +53,9 @@ public class AdminBuyFormController {
         }
         if (!buyFormService.checkBuyForm(buyForm))
             return new ResponseEntity<>("check fail", HttpStatus.BAD_REQUEST);
+        userMail.setEmailAddress(buyForm.getEmail());
+        String content = "Đơn hàng của bạn đã được duyệt. Chúng tôi sẽ giao hàng trong thời gian sớm nhất";
+        sendMailService.sendMail(userMail,"Thông báo đặt mua hàng",content);
         return new ResponseEntity<>("check success", HttpStatus.OK);
     }
 }
